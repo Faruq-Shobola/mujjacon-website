be356eed390778cd8fb3ec8179176a039a485805
unknown <shobolafaruq@gmail.com>
2022-05-17 17:53:19 +0100

    explore page layout completed

---
+++ theme/page-explore.php
@@ -0,0 +1,27 @@
+<?php get_header() ?>
+
+<section class="hero top-0 relative flex justify-center items-center overflow-hidden">
+    <!------------ PAGE TITLE ------------>
+    <div class="md:max-w-3xl mx-auto px-4">
+        <div class="flex flex-col gap-8 text-center text-white">
+            <h2 class="capitalize font-bold text-4xl md:text-5xl leading-tight md:leading-snug font-head">Growing everyday business with innovative digital solutions</h2>
+            <!------------ BUTTON ------------>
+            <a href="/get-quote" class="bg-red-500 w-40 py-3 mx-auto text-center text-white font-semibold capitalize font-body">Get started</a>
+        </div>
+    </div>
+</section>
+<!-------------------------------- 
+/// 02   GALLERY SECTION
+---------------------------------->
+<section class="">
+    <div class="max-w-6xl mx-auto py-20 px-4 lg:px-8">
+        <!------------  ------------>
+        <div class="grid">
+            
+
+        
+        </div>
+    </div>
+</section>
+
+<?php get_footer() ?>
