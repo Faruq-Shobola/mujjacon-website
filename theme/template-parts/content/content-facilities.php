3467d593071005263e94bcb7f1bc28d304f23bdc
unknown <shobolafaruq@gmail.com>
2022-05-19 06:43:04 +0100

    adding icons

--- theme/template-parts/content/content-facilities.php
+++ theme/template-parts/content/content-facilities.php
@@ -1,6 +1,6 @@
 
            
-           <div class="w-full bg-white rounded drop-shadow-sm  overflow-hidden">
+           <div class="w-full bg-white rounded-lg drop-shadow-sm  overflow-hidden">
                 <?php if(has_post_thumbnail()):?>
                     <img src="<?php the_post_thumbnail_url();?>" alt="<?php the_title();?>" class="object-cover w-full h-64" />
                 <?php endif;?>
@@ -8,11 +8,27 @@
                    <h3 class="mt-4 text-xl font-medium text-light-green">
                     <?php the_title();?>
                    </h3>
-                   <p class="py-2 text-base text-black-800">
-                       Ori-Okuta, Atan, Otta, Ogun State
-                   </p>
-                   <p class="text-base">
-                       land size: 10.67 sq ft
-                   </p>
+                   <div class="flex gap-3 items-center py-2">
+                        <div class="">
+                            <span class="flex justify-center items-center">
+                                <i class="fas fa-map-marker-alt text-dark-green"></i>
+                            </span>
+                        </div>
+                        <div class="">
+                                <p class="capitalize text-base text-black"> <?php the_field('location') ?> </p>
+                        </div>
+                    </div>
+                    <div class="flex gap-3 items-center py-2">
+                        <div class="">
+                            <span class="flex justify-center items-center">
+                                <i class="fas fa-ruler-triangle text-dark-green"></i>
+                                <i class="fas fa-ruler-triangle"></i>
+                            </span>
+                        </div>
+                        <div class="">
+                                <p class="capitalize text-base text-black"> <?php the_field('size') ?> </p>
+                        </div>
+                    </div>
+                   
                </div>
            </div>     
